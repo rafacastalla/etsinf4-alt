926a89128bc7b58bb5671303d4f6f30ba1ac1454
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-10-05 22:16:15 +0200

    Práctica 1: ejercicio opcional completado

--- practica1.py
+++ practica1.py
@@ -57,5 +57,31 @@ def ejercicio4space(l):
     a.reverse();
     return a;
 
+def menores(l):
+    men = [];
+
+    for i in l:
+        if len(men) == 0 or i > men[-1]:
+            men.append(i);
+        else:
+            a = 0;
+            b = len(men);
+            found = False;
+            while not found and a + 1 != b:
+                c = int((a + b) / 2);
+                if (i > men[c]):
+                    a = c;
+                elif (i < men[c]):
+                    b = c;
+                else:
+                    found = True;
+
+            if not found:
+                if a == 0 and (len(men) == 1 or men[a] > i):
+                        men[a] = i;
+                else:
+                    men[b] = i;
+    return men;
+
 a = [210, 816, 357, 107, 889, 635, 733, 930, 842, 542];
 
