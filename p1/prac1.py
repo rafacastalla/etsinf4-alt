935747872aacda22656235a8ae56b1c68814dc39
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-10-10 09:32:13 +0200

    P1: added headers and split file

--- p1/prac1.py
+++ p1/prac1.py
@@ -1,3 +1,13 @@
+'''
+Práctica 1 ALT, ejercicios 2 al 4
+
+El ejercicio 4 presenta 2 versiones: una optimizada para emplear menos tiempo
+y otra que minimiza la memoria empleada
+
+Autor: Carlos Santiago Galindo Jiménez
+Autor: José Antonio Pérez
+'''
+
 def ejercicio2(l):
     res = [];
     for i in range(0,len(l)):
@@ -55,32 +65,6 @@ def ejercicio4space(l):
 
     a.reverse();
     return a;
-
-def menores(l):
-    men = [];
-
-    for i in l:
-        if len(men) == 0 or i > men[-1]:
-            men.append(i);
-        else:
-            a = 0;
-            b = len(men);
-            found = False;
-            while not found and a + 1 != b:
-                c = int((a + b) / 2);
-                if (i > men[c]):
-                    a = c;
-                elif (i < men[c]):
-                    b = c;
-                else:
-                    found = True;
-
-            if not found:
-                if a == 0 and (len(men) == 1 or men[a] > i):
-                        men[a] = i;
-                else:
-                    men[b] = i;
-    return len(men);
 
 a = [210, 816, 357, 107, 889, 635, 733, 930, 842, 542];
 
