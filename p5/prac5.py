b058e57c34c137710b5de930a085f641f23d8e10
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-12-20 09:02:28 +0100

    p5: ejercicio3 completado

--- p5/prac5.py
+++ p5/prac5.py
@@ -1,5 +1,6 @@
 import numpy as np
 import heapq # a priority queue
+import math
 
 def create_graph(N,maxvalue=1000):
   G = np.random.randint(maxvalue, size=(N, N))
@@ -10,6 +11,18 @@ def create_graph(N,maxvalue=1000):
       G[i][j] -= m
       G[j][i] -= m
   return G
+
+def process_graph(G):
+  # EJERCICIO 2
+  N = G.shape[0]
+  for x in range(0, N-1):
+    for y in range(x+1, N):
+      if (G[x, y] > G[y, x]):
+        G[x, y] -= G[y, x]
+        G[y, x] = 0
+      else:
+        G[y, x] -= G[x, y]
+        G[x, y] = 0
 
 def generate_random_ordering(G):
   # let's assume that G is a square Numpy matrix of integers
@@ -63,20 +76,23 @@ def BranchAndBound(G):
   # ojo con graph <- "se mira pero no se toca"
   N = G.shape[0]
 
-  def optimistic(s):
+  def optimistic(s, p_score = float('nan')):
     # s es una lista de vertices entre 0 y N-1
-    opt = 0
     # Vertices no colocados en s
     rest = [i for i in range(N) if i not in s]
-    # Aristas de vertices colocados a desconocidos
-    opt += sum(G[i,j] for i in s for j in rest)
-    opt -= sum(G[j,i] for i in s for j in rest)
-    # Aristas entre vertices colocados
-    opt += sum(G[i,j] for x,i in enumerate(s) for y,j in enumerate(s) if x < y)
-    opt -= sum(G[j,i] for x,i in enumerate(s) for y,j in enumerate(s) if x < y)
-    # Estimacion aristas entre vertices desconocidos (suma de todos sus pesos)
-    opt += sum(G[i,j] + G[j,i] for i in rest for j in rest)
-    return opt
+    if math.isnan(p_score):
+      opt = 0
+      # Aristas de vertices colocados a desconocidos
+      opt += sum(G[i,j] for i in s for j in rest)
+      opt -= sum(G[j,i] for i in s for j in rest)
+      # Aristas entre vertices colocados
+      opt += sum(G[i,j] for x,i in enumerate(s) for y,j in enumerate(s) if x < y)
+      opt -= sum(G[j,i] for x,i in enumerate(s) for y,j in enumerate(s) if x < y)
+      # Estimacion aristas entre vertices desconocidos (suma de todos sus pesos)
+      opt += sum(G[i,j] for i in rest for j in rest)
+      return opt
+    else:
+      return p_score - 2 * sum(G[i,s[-1]] for i in rest)
 
   def branch(s):
     return (s+[i] for i in range(N) if i not in s)
@@ -104,18 +120,16 @@ def BranchAndBound(G):
     print("Iter. %04d |A|=%05d max|A|=%05d fx=%04d score_s=%04d" % \
       (iter,lenA,maxA,fx,score_s))
     for child in branch(s):
-      fchild = optimistic(child)
+      fchild = optimistic(child, score_s)
       if is_complete(child): # si es terminal
         # seguro que es factible
         # falta ver si mejora la mejor solucion en curso
-        # COMPLETAR
         if fchild > fx:
           x = child
           fx = fchild
       else: # no es terminal
         # lo metemos en el cjt de estados activos si supera
         # la poda por cota optimista:
-        # COMPLETAR
         if fchild > fx:
           heapq.heappush(A,(-fchild, child))
   return x,fx
@@ -123,6 +137,11 @@ def BranchAndBound(G):
 if __name__ == "__main__":
     N = 8
     G = create_graph(N,5)
+#     G = np.asarray([[0, 1, 1, 1],
+#                     [0, 0, 1, 1],
+#                     [0, 0, 0, 1],
+#                     [0, 0, 0, 0]], dtype=np.int)
+    process_graph(G)
     print("G=",G)
     x,fx = BranchAndBound(G)
     greedy_ordering = generate_greedy_ordering(G)
